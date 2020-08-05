bfbe3eb5c5e94e1d375f6ca52aa3f5e912ff3bc9
plusha <valera_pon@mail.ru>
2020-08-05 23:07:55 +0600

    commit

--- main.py
+++ main.py
@@ -4,10 +4,24 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 
+from PIL import Image
+
 # X = torch.tensor([1, 2])
 # Y = torch.randn(5, 5, device='cuda', dtype=float)
 
 # print(X)
 # print(Y)
-print(os.getcwd())
-torchvision.datasets.MNIST(root=os.getcwd(), train=True, download=True)
+# print(os.getcwd())
+# torchvision.datasets.MNIST(root=os.getcwd(), train=True, download=True)
+
+train_loader = torch.utils.data.DataLoader(
+        torchvision.datasets.MNIST(
+                root=os.getcwd(),
+                train=True,
+                download=True
+        ),
+        batch_size=64,
+        shuffle=True
+)
+
+print(train_loader)
