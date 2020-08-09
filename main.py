43ce37d53002a8a01f0388b313cba8e5fce68663
plusha <valera_pon@mail.ru>
2020-08-09 16:43:09 +0600

    pre-finall version

--- main.py
+++ main.py
@@ -27,8 +27,39 @@ class Network(nn.Module):
                 x = F.relu(self.fc1(x))
                 x = F.dropout(x, training=self.training)
                 x = self.fc2(x)
-                return F.log_softmax(x)
+                return F.log_softmax(x, -1)
                 
+
+def train(network, train_loader, optimizer, epoch):
+        network.train()
+        for batch_idx, (data, target) in enumerate(train_loader):
+
+                data = data.cuda()
+                target = target.cuda()
+
+                optimizer.zero_grad()
+                output = network.forward(data)
+                loss = F.cross_entropy(output, target)
+                # loss = F.nll_loss(output, target)
+                loss.backward()
+                optimizer.step()
+                # if batch_idx % 10 == 0:
+                #         print('Train epoch: %s, {%s}, Loss: %s' % (epoch, batch_idx, loss.item()))
+
+def test(network, test_loader):
+        test_loss = 0
+
+        network.eval()
+        with torch.no_grad():
+                for data, target in enumerate(test_loader):
+                        output = network.forward(data)
+                        test_loss += F.nll_loss(output, target, size_average=False).item()
+                test_loss /= len(test_loader.dataset)
+                print(test_loader)
+                        # pred = output.data.max(1, keepdim=True)[1]
+                        # correct += pred.eq(target.data.view_as(pred)).sum()
+
+
 
 if __name__ == '__main__':
         train_loader = torch.utils.data.DataLoader(
@@ -57,7 +88,23 @@ if __name__ == '__main__':
                 shuffle=True
         )
 
-        Net = Network()
+        Net = Network().to(device='cuda')
         optimizer = optim.SGD(Net.parameters(), lr=0.01, momentum=0.5)
+
+        for i in range(3):
+                train(Net, train_loader, optimizer, i)
+
+        i, (data, target) = next(enumerate(train_loader))
+        # print(data[0])
+        transforms.ToPILImage()(data[0]).show()
+        print(target)
+        # test(Net, test_loader)
+
+        Net.eval()
+        print(Net.forward(data[:1].cuda()))
+
+
+
+
 
 
